213b3e0b49d4f151309fd6595705c8a8f0ebde8c
nico703 <Schnittig.kantern.06@icloud.com>
2026-08-23 20:59:14 +0200

    Jugend Trainingszeit und Trainer aktualisiert

--- src/data/jugend.ts
+++ src/data/jugend.ts
@@ -28,32 +28,19 @@ export const jugendTeams: JugendTeam[] = [
 	},
 	{
 		name: "G1-Jugend",
-		jahrgaenge: "Jahrgang 2019",
-		trainer: "Anton, Nils",
-		kontaktMail: "jugend@tsg-messel.de",
-		slots: [
-			{
-				wochentag: "Donnerstag",
-				uhrzeit: "17:00 – 18:30",
-				platz: "",
-			},
-		],
-	},
-	{
-		name: "G2-Jugend",
 		jahrgaenge: "Jahrgang 2020",
 		trainer: "Senol, Sophie",
 		kontaktMail: "jugend@tsg-messel.de",
 		slots: [
 			{
 				wochentag: "Freitag",
-				uhrzeit: "16:00 – 17:00",
+				uhrzeit: "16:45 – 17:45",
 				platz: "",
 			},
 		],
 	},
 	{
-		name: "G3-Jugend",
+		name: "G2-Jugend",
 		jahrgaenge: "Jahrgang 2021",
 		trainer: "Dominik",
 		kontaktMail: "jugend@tsg-messel.de",
@@ -67,44 +54,44 @@ export const jugendTeams: JugendTeam[] = [
 	},
 	{
 		name: "F1-Jugend",
-		jahrgaenge: "Jahrgang 2017",
-		trainer: "Corinna, Norman",
+		jahrgaenge: "Jahrgang 2018",
+		trainer: "Dejan, Jojo, Jörn",
 		kontaktMail: "jugend@tsg-messel.de",
 		slots: [
 			{
 				wochentag: "Dienstag",
-				uhrzeit: "17:00 – 18:30",
+				uhrzeit: "17:30 – 18:45",
 				platz: "",
 			},
 			{
 				wochentag: "Donnerstag",
-				uhrzeit: "17:00 – 18:30",
+				uhrzeit: "17:30 – 18:45",
 				platz: "",
 			},
 		],
 	},
 	{
 		name: "F2-Jugend",
-		jahrgaenge: "Jahrgang 2018",
-		trainer: "Jörn, Dejan, Jojo",
+		jahrgaenge: "Jahrgang 2019",
+		trainer: "Anton, Nils",
 		kontaktMail: "jugend@tsg-messel.de",
 		slots: [
 			{
-				wochentag: "Dienstag",
-				uhrzeit: "17:30 – 18:45",
+				wochentag: "Montag",
+				uhrzeit: "17:00 – 18:30",
 				platz: "",
 			},
 			{
 				wochentag: "Donnerstag",
-				uhrzeit: "17:30 – 18:45",
+				uhrzeit: "17:00 – 18:00",
 				platz: "",
 			},
 		],
 	},
 	{
 		name: "E1-Jugend",
-		jahrgaenge: "Jahrgang 2015",
-		trainer: "Jonas, Matthias, Ray",
+		jahrgaenge: "Jahrgang 2016",
+		trainer: "Tom, Iven",
 		kontaktMail: "jugend@tsg-messel.de",
 		slots: [
 			{
@@ -121,17 +108,17 @@ export const jugendTeams: JugendTeam[] = [
 	},
 	{
 		name: "E2-Jugend",
-		jahrgaenge: "Jahrgang 2016",
-		trainer: "Iven, Tom",
+		jahrgaenge: "Jahrgang 2017",
+		trainer: "Norman, Corinna",
 		kontaktMail: "jugend@tsg-messel.de",
 		slots: [
 			{
-				wochentag: "Montag",
+				wochentag: "Dienstag",
 				uhrzeit: "17:00 – 18:30",
 				platz: "",
 			},
 			{
-				wochentag: "Mittwoch",
+				wochentag: "Donnerstag",
 				uhrzeit: "17:00 – 18:30",
 				platz: "",
 			},
@@ -139,18 +126,18 @@ export const jugendTeams: JugendTeam[] = [
 	},
 	{
 		name: "D-Jugend",
-		jahrgaenge: "Jahrgänge 2013–2014",
-		trainer: "Tobias, Dominic",
+		jahrgaenge: "Jahrgänge 2014/15",
+		trainer: "Philipp, Robin",
 		kontaktMail: "jugend@tsg-messel.de",
 		slots: [
 			{
 				wochentag: "Dienstag",
-				uhrzeit: "17:15 – 18:45",
+				uhrzeit: "17:15 – 18:40",
 				platz: "",
 			},
 			{
 				wochentag: "Donnerstag",
-				uhrzeit: "17:15 – 18:45",
+				uhrzeit: "17:15 – 18:40",
 				platz: "",
 			},
 		],
@@ -158,6 +145,24 @@ export const jugendTeams: JugendTeam[] = [
 	{
 		name: "C-Junioren",
 		jahrgaenge: "Jahrgänge 2011–2012",
+		trainer: "Kevin, Robert",
+		kontaktMail: "jugend@tsg-messel.de",
+		slots: [
+			{
+				wochentag: "Mittwoch",
+				uhrzeit: "18:00 – 19:30",
+				platz: "",
+			},
+			{
+				wochentag: "Freitag",
+				uhrzeit: "18:00 – 19:30",
+				platz: "",
+			},
+		],
+	},
+	{
+		name: "B-Junioren",
+		jahrgaenge: "Jahrgänge 2010/11",
 		trainer: "Kevin, Robert",
 		kontaktMail: "jugend@tsg-messel.de",
 		slots: [
